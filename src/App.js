f364687690f213d0b63fe8652e77bec379baec4e
Markus Mengel <markus.mengelhx@gmail.com>
2020-08-05 15:10:36 +0200

    removed react logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -11,3 +10,4 @@ class App extends Component {
 }
 
 export default App;
+
