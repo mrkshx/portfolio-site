37bffcdecfda474aa4f900a3c5e583114561e82d
Markus Mengel <markus.mengelhx@gmail.com>
2020-08-05 16:09:02 +0200

    created basic mavbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import './App.css';
 
 class App extends Component {
@@ -9,8 +10,16 @@ class App extends Component {
     return (
       <Router>
         <Container fluid={true}>
-          <Navbar className="border-bottom">
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
             <Navbar.Brand>Markus Mengel</Navbar.Brand>
+            <Navbar.Toggle className="border-0" aria-control="navbar-toggle"/>
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
           </Navbar>
         </Container>
       </Router>
