e5ddf1ff7de9353e2a43efc47f6bf6bec030f4a5
Markus Mengel <markus.mengelhx@gmail.com>
2020-08-06 18:16:31 +0200

    filled footer with 3 links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 function Footer() {
 
@@ -10,10 +12,19 @@ function Footer() {
       <Container fluid={true} >
         <Row className="border-top p-4 justify-content-between" >
           <Col>
-            test
+            <a href="https://www.linkedin.com/in/markus-mengel" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={['fab', 'linkedin']} size="3x"/>
+            </a>
           </Col>
           <Col>
-            test
+            <a href="https://github.com/mrkshx/" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={['fab', 'github']} size="3x"/>
+            </a>
+          </Col>
+          <Col>
+            <a href="https://www.xing.com/profile/Markus_Mengel3" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={['fab', 'xing']} size="3x"/>
+            </a>
           </Col>
         </Row>
       </Container>
