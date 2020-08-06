bb82019a8bd4b70f559884674042e247887e0298
Markus Mengel <markus.mengelhx@gmail.com>
2020-08-06 17:39:51 +0200

    added colors to skill and showing them when hovering over it

--- src/components/skill_card.js
+++ src/components/skill_card.js
@@ -4,9 +4,17 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function SkillCard(props) {
 
+  const handleMouseEnter = (e) => {
+    e.target.style.color=props.skill.color;
+      };
+
+  const handleMouseLeave = (e) => {
+    e.target.style.color='black';
+      };
+
   return(
     <div>
-      <FontAwesomeIcon icon={[props.skill.symboltype, props.skill.symbol]} size="6x"/>
+      <FontAwesomeIcon icon={[props.skill.symboltype, props.skill.symbol]} size="6x" onMouseOver={handleMouseEnter} onMouseLeave={handleMouseLeave}/>
     </div>
   );
 }
--- src/constants/skills.js
+++ src/constants/skills.js
@@ -2,27 +2,32 @@ const skills_array = [
   {
     name: 'Javascript',
     symbol: 'js',
-    symboltype: 'fab'
+    symboltype: 'fab',
+    color: '#f1da1c'
   },
   {
     name: 'Ruby',
     symbol: 'gem',
-    symboltype: 'fas'
+    symboltype: 'fas',
+    color: '#e94b1f'
   },
   {
     name: 'HTML',
     symbol: 'html5',
-    symboltype: 'fab'
+    symboltype: 'fab',
+    color: '#e86126'
   },
   {
     name: 'CSS',
     symbol: 'css3',
-    symboltype: 'fab'
+    symboltype: 'fab',
+    color: '#2f5de0'
   },
   {
     name: 'React',
     symbol: 'react',
-    symboltype: 'fab'
+    symboltype: 'fab',
+    color: '#68e1fd'
   }
 ];
 
