f9a00094917d9a3cf025a14ebd24453317dcc0f5
Markus Mengel <markus.mengelhx@gmail.com>
2020-08-13 20:18:27 +0200

    imported sample cloudinary image

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import {Image} from 'cloudinary-react';
 
 import Hero from '../components/hero';
 
@@ -10,6 +11,7 @@ function AboutPage(props) {
         <div>
             <Hero title={props.title} />
             <Container>
+              <Image className="mb-5" cloudName="demo" publicId="sample" width="250" height="250" crop="scale" radius="max" />
               <p>Hey, I'm Markus. This year I started my journey a the career path Software Development.</p>
               <p>So I joined an onsite Bootcamp for Web Development where I learnt Ruby, Ruby on Rails, Javascript, Bootstrap, Heroku, etc.</p>
               <p>Right after that, I attended another Bootcamp online. This course teached me how to develop Mobile Applications with Flutter and Dart. I also got insights in Firebase</p>
