b913d6d5cccfee8630e57d91b02eb5241a9184ce
Markus Mengel <markus.mengelhx@gmail.com>
2020-08-17 19:37:13 +0200

    typo first intro sentence

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -14,7 +14,7 @@ function AboutPage(props) {
             <Hero title={props.title} />
             <Container>
               <Image className="avatar mb-5" cloudName={process.env.REACT_APP_CLOUDINARY_CLOUDNAME} publicId="avatar_i7fl5h" width="250" height="250" crop="thumb" gravtity="face-center" radius="max" />
-              <p>Hey, I'm Markus. This year I started my journey a the career path Software Development.</p>
+              <p>Hey, I'm Markus. This year I started my journey in the career path of Software Development.</p>
               <p>So I joined an onsite Bootcamp for Web Development where I learnt Ruby, Ruby on Rails, Javascript, Bootstrap, Heroku, etc.</p>
               <p>Right after that, I attended another Bootcamp online. This course teached me how to develop Mobile Applications with Flutter and Dart. I also got insights in Firebase</p>
               <p>I have a long list of what skills I want to learn in the future. Currently I'm learning React and Redux.</p>
