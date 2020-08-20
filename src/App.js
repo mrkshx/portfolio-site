e36a1227414dfd8a56c19c70ee5d2410ebffc847
Markus Mengel <markus.mengelhx@gmail.com>
2020-08-20 20:23:54 +0200

    replaced default collapse_symbol by fontawesome_icon

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import './assets/style/colors.scss';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import skills_array from './constants/skills';
 import HomePage from './pages/HomePage';
@@ -44,7 +45,11 @@ class App extends Component {
             <Navbar.Brand>
               <Link className="nav-link" to="/">Markus Mengel</Link>
             </Navbar.Brand>
-            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle"/>
+            <Navbar.Toggle aria-controls="basic-navbar-nav">
+              <span>
+                <FontAwesomeIcon id="navbar-hamburger" icon="bars"/>
+              </span>
+            </Navbar.Toggle>
             <Navbar.Collapse id="navbar-toggle">
               <Nav className="ml-auto">
                 <Link className="nav-link" to="/">Home</Link>
