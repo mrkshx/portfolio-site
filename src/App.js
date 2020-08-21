3a331997a108e85101b1fd98b1fbbefbbef8df4d
Markus Mengel <markus.mengelhx@gmail.com>
2020-08-21 16:12:03 +0200

    removed app.css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import './App.css';
 import './assets/style/navbar.scss';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
