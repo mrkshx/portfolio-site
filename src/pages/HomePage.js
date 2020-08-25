05b9bcc99d41bdafa5e9d739d3bcb977bac82e33
Markus Mengel <markus.mengelhx@gmail.com>
2020-08-25 14:54:28 +0200

    commented skillcards and hero out on Homepage and add Card with image

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Card from 'react-bootstrap/Card';
 
 import Hero from '../components/hero';
 import SkillCard from '../components/skill_card';
+import free_image from '../assets/images/12836.jpg';
 
 function HomePage(props) {
 
@@ -16,12 +18,24 @@ function HomePage(props) {
 
   return(
     <div>
-      <Hero title={props.title} subtitle={props.subtitle} note={props.note}></Hero>
-      <Container fluid={true} >
-        <Row className='justify-content-around'>
-          {showSkills()}
-        </Row>
-      </Container>
+      <Card className="bg-dark text-white">
+        <Card.Img id="landing-img"  src={free_image} alt="Landingpage Image" />
+        <Card.ImgOverlay>
+          <Card.Title>{props.title}</Card.Title>
+          <Card.Text>
+            {props.subtitle}
+          </Card.Text>
+          <Card.Text>{props.note}</Card.Text>
+        </Card.ImgOverlay>
+      </Card>
+      {/*
+            <Hero title={props.title} subtitle={props.subtitle} note={props.note}></Hero>
+            <Container fluid={true} >
+              <Row className='justify-content-around'>
+                {showSkills()}
+              </Row>
+            </Container>
+      */}
     </div>
   );
 }
