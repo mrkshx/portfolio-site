1ac99bed039c03bee63da44c2da6bae90a25adda
Markus Mengel <markus.mengelhx@gmail.com>
2020-08-27 15:33:21 +0200

    fixed responsiveness for portrait mode

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -18,7 +18,7 @@ function HomePage(props) {
           <Card.Text> Hello, I'm </Card.Text>
           <Card.Header as="h1">{props.name}</Card.Header>
           <Card.Header as="h3">{props.title}</Card.Header>
-          <Card.Text>{props.subtitle}</Card.Text>
+          <Card.Text id="landing-card-subtitle" >{props.subtitle}</Card.Text>
           <Button className="landing-btn" variant="info" size="lg" href="#">My Projects</Button>
           <Button className="landing-btn" variant="success" size="lg" href="/contact">Contact Me</Button>
           <Image  id="landing-avatar" cloudName={process.env.REACT_APP_CLOUDINARY_CLOUDNAME} publicId="landing_avatar_oerydq" width="350" height="350" radius="max" />
