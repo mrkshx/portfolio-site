a509b430a44dc868b6040605c8ea3927561c52a1
Markus Mengel <markus.mengelhx@gmail.com>
2020-08-28 13:25:53 +0200

    created new projectpage incl. routing and link

---
+++ src/pages/ProjectPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import '../assets/style/projectpage.scss';
+
+
+function ProjectPage(props) {
+
+  return(
+      <div id="projectpage" >
+        <p>This is the ProjectPage!</p>
+      </div>
+  );
+}
+
+export default ProjectPage;
