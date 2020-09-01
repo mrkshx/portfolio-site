e2ee4d3d4e37dd8aa2810a96bda253329f4cb9b2
Markus Mengel <markus.mengelhx@gmail.com>
2020-09-01 11:24:42 +0200

    added more style to construction banner

--- src/components/construction.js
+++ src/components/construction.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import '../assets/style/construction.scss';
 
 function ConstructionBanner(props) {
 
   return(
       <Container>
-        <Row className="justify-content-center" >
-            <FontAwesomeIcon icon={['fas', 'wrench']} color="grey" size="3x" flip="horizontal"/>
+        <Row className="construction justify-content-around align-items-center" >
+          <Col className="construction-item">
+            <FontAwesomeIcon icon={['fas', 'wrench']} color="grey" size="5x" flip="horizontal"/>
+          </Col>
+          <Col className="construction-item">
             <p> This page is still under construction! </p>
-            <FontAwesomeIcon icon={['fas', 'wrench']} color="grey" size="3x" />
+            <p> You will find my projects here that I worked on. </p>
+          </Col>
+          <Col className="construction-item">
+            <FontAwesomeIcon icon={['fas', 'wrench']} color="grey" size="5x" />
+          </Col>
         </Row>
       </Container>
   );
