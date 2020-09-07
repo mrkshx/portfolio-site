be4124eb89877d88c7871c9176f708cb55eee77e
Markus Mengel <markus.mengelhx@gmail.com>
2020-09-07 14:26:39 +0200

    minimal creation of new mobile_project_card without any styling

---
+++ src/components/mobile_project_card.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import {Image} from 'cloudinary-react';
+
+import '../assets/style/mobile_project_card.scss';
+
+
+function MobileProjectCard(props) {
+
+  return(
+    <Card className="mobile-project-card" text="white" >
+      <Image className="card-img mobile-project-image" cloudName={process.env.REACT_APP_CLOUDINARY_CLOUDNAME} publicId={props.image} width="320" />
+      <Card.ImgOverlay className="mobile-project-card-img-overlay" >
+        <div className="mobile-project-card-text" >
+          <Card.Title>{props.name}</Card.Title>
+          <Card.Text>{props.description}</Card.Text>
+        </div>
+      </Card.ImgOverlay>
+    </Card>
+  );
+}
+
+export default MobileProjectCard;
