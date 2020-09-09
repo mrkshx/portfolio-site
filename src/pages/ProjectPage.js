fc8d6d7a1a85bf3184ef280b22bfb57789299da7
Markus Mengel <markus.mengelhx@gmail.com>
2020-09-09 20:21:04 +0200

    converted projectpage to class component

--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import firebase from "../FireStore";
 import Container from 'react-bootstrap/Container';
 
@@ -8,9 +8,15 @@ import MobileProjectCard from '../components/mobile_project_card';
 
 import '../assets/style/projectpage.scss';
 
-function ProjectPage(props) {
+class ProjectPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: ""
+    }
+  }
 
-  const showProjects = () => {
+  showProjects = () => {
     const db = firebase.firestore();
     db.collection("projects").get().then((querySnapshot) => {
       querySnapshot.forEach((project) => {
@@ -26,45 +32,46 @@ function ProjectPage(props) {
     })
   }
 
-  return(
-      <div id="projectpage" >
-        <Hero title={props.title} ></Hero>
-        <Container>
-          <h4>Web Apps</h4>
-          {showProjects()}
-          <ProjectCard
-            name="Cars B'n'B"
-            description="A simple car booking application with functionality to review a booking"
-            url="https://cars-bnb.herokuapp.com/"
-            image="https://source.unsplash.com/axTvnbgRlAk"
-            skills={[['fas', 'gem'],['fab', 'html5'],['fab', 'css3'],['fab', 'js']]}
-          />
-        </Container>
-        <Container>
-          <h4>Mobile Apps</h4>
-          <div id="mobile_cards" >
-            <MobileProjectCard
-              name="BMI Calculator"
-              description="Give you info about your bmi depending on your input"
-              image="portfolio/bmi_static_tauwhu"
-              gif="portfolio/bmi_w320_mbjafj"
+  render() {
+    return(
+        <div id="projectpage" >
+          <Hero title={this.props.title} ></Hero>
+          <Container>
+            <h4>Web Apps</h4>
+            <ProjectCard
+              name="Cars B'n'B"
+              description="A simple car booking application with functionality to review a booking"
+              url="https://cars-bnb.herokuapp.com/"
+              image="https://source.unsplash.com/axTvnbgRlAk"
+              skills={[['fas', 'gem'],['fab', 'html5'],['fab', 'css3'],['fab', 'js']]}
             />
-            <MobileProjectCard
-              name="Todoey"
-              description="This is a todo list. You can check a task as done and delete it"
-              image="portfolio/todoey_static_ryxy45"
-              gif="portfolio/todoey_w320_qegqgw"
-            />
-            <MobileProjectCard
-              name="Clima"
-              description="Show weather info depending on GPS or manual input"
-              image="portfolio/clima_static_cgrnu6"
-              gif="portfolio/clima_w320_xpsfvm"
-            />
-          </div>
-        </Container>
-      </div>
-  );
+          </Container>
+          <Container>
+            <h4>Mobile Apps</h4>
+            <div id="mobile_cards" >
+              <MobileProjectCard
+                name="BMI Calculator"
+                description="Give you info about your bmi depending on your input"
+                image="portfolio/bmi_static_tauwhu"
+                gif="portfolio/bmi_w320_mbjafj"
+              />
+              <MobileProjectCard
+                name="Todoey"
+                description="This is a todo list. You can check a task as done and delete it"
+                image="portfolio/todoey_static_ryxy45"
+                gif="portfolio/todoey_w320_qegqgw"
+              />
+              <MobileProjectCard
+                name="Clima"
+                description="Show weather info depending on GPS or manual input"
+                image="portfolio/clima_static_cgrnu6"
+                gif="portfolio/clima_w320_xpsfvm"
+              />
+            </div>
+          </Container>
+        </div>
+    );
+  }
 }
 
 export default ProjectPage;
