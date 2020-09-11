f17fa1b43ffa997226ddd1a0d0845286ede7efe8
Markus Mengel <markus.mengelhx@gmail.com>
2020-09-12 01:48:33 +0200

    removed image type and renamed image as of renaming in cloudinary

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -12,9 +12,9 @@ function HomePage(props) {
     <div id="homepage" >
       <Card id="landing-card" className="bg-dark">
         <picture>
-          <source srcSet="https://res.cloudinary.com/dyrcmbg1b/image/upload/v1599827809/portfolio/12836_rcdz86.webp" type="image/webp" />
-          <source srcSet="https://res.cloudinary.com/dyrcmbg1b/image/upload/v1598405036/portfolio/12836_jfldf1.jpg" type="mage/jpeg" />
-          <Image  className="card-img" id="landing-img" cloudName={process.env.REACT_APP_CLOUDINARY_CLOUDNAME} publicId="portfolio/12836_jfldf1" alt="Landing Image"/>
+          <source srcSet="https://res.cloudinary.com/dyrcmbg1b/image/upload/v1599827809/portfolio/landing_image_webp" />
+          <source srcSet="https://res.cloudinary.com/dyrcmbg1b/image/upload/v1598405036/portfolio/landing_image_jpg"/>
+          <Image  className="card-img" id="landing-img" cloudName={process.env.REACT_APP_CLOUDINARY_CLOUDNAME} publicId="portfolio/landing_image_jpg" alt="Landing Image"/>
         </picture>
         <Card.ImgOverlay id="landing-card-overlay" >
           <Card.Text> Hello, I'm </Card.Text>
@@ -24,9 +24,9 @@ function HomePage(props) {
           <Button className="landing-btn" variant="info" size="lg" href="/projects">My Projects</Button>
           <Button className="landing-btn" variant="success" size="lg" href="/contact">Contact Me</Button>
           <picture>
-            <source srcSet="https://res.cloudinary.com/dyrcmbg1b/image/upload/w_350,h_350/v1599827809/portfolio/landing_avatar_j85x2i.webp" type="image/webp" />
-            <source srcSet="https://res.cloudinary.com/dyrcmbg1b/image/upload/w_350,h_350/v1598405036/portfolio/landing_avatar_oerydq.jpg" type="image/jpeg" />
-            <Image id="landing-avatar" cloudName={process.env.REACT_APP_CLOUDINARY_CLOUDNAME} publicId="portfolio/landing_avatar_oerydq" alt="Landing Avatar"width="350" height="350" radius="max" />
+            <source srcSet="https://res.cloudinary.com/dyrcmbg1b/image/upload/w_350,h_350/v1599827809/portfolio/landing_avatar_webp" />
+            <source srcSet="https://res.cloudinary.com/dyrcmbg1b/image/upload/w_350,h_350/v1598405036/portfolio/landing_avatar_jpg" />
+            <Image id="landing-avatar" cloudName={process.env.REACT_APP_CLOUDINARY_CLOUDNAME} publicId="portfolio/landing_avatar_jpg" alt="Landing Avatar"width="350" height="350" radius="max" />
           </picture>
         </Card.ImgOverlay>
       </Card>
