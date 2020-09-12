de76feb6b72bb0448784c7295b63c8334ab2cb35
Markus Mengel <markus.mengelhx@gmail.com>
2020-09-13 00:07:56 +0200

    home_page: removing picture element again and use transformation tag with f_auto and q_auto

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-import {Image} from 'cloudinary-react';
+import { Image, Transformation } from 'cloudinary-react';
 
 import '../assets/style/homepage.scss';
 
@@ -11,11 +11,9 @@ function HomePage(props) {
   return(
     <div id="homepage" >
       <Card id="landing-card" className="bg-dark">
-        <picture>
-          <source srcSet="https://res.cloudinary.com/dyrcmbg1b/image/upload/v1599827809/portfolio/landing_image_webp" />
-          <source srcSet="https://res.cloudinary.com/dyrcmbg1b/image/upload/v1598405036/portfolio/landing_image_jpg"/>
-          <Image  className="card-img" id="landing-img" cloudName={process.env.REACT_APP_CLOUDINARY_CLOUDNAME} publicId="portfolio/landing_image_jpg" alt="Landing Image"/>
-        </picture>
+        <Image className="card-img" id="landing-img" cloudName={process.env.REACT_APP_CLOUDINARY_CLOUDNAME} publicId="portfolio/landing_image_jpg" alt="Landing Image">
+          <Transformation quality="auto" fetchFormat="auto" />
+        </Image>
         <Card.ImgOverlay id="landing-card-overlay" >
           <Card.Text> Hello, I'm </Card.Text>
           <Card.Header as="h1">{props.name}</Card.Header>
@@ -23,11 +21,9 @@ function HomePage(props) {
           <Card.Text id="landing-card-subtitle" >{props.subtitle}</Card.Text>
           <Button className="landing-btn" variant="info" size="lg" href="/projects">My Projects</Button>
           <Button className="landing-btn" variant="success" size="lg" href="/contact">Contact Me</Button>
-          <picture>
-            <source srcSet="https://res.cloudinary.com/dyrcmbg1b/image/upload/w_350,h_350/v1599827809/portfolio/landing_avatar_webp" />
-            <source srcSet="https://res.cloudinary.com/dyrcmbg1b/image/upload/w_350,h_350/v1598405036/portfolio/landing_avatar_jpg" />
-            <Image id="landing-avatar" cloudName={process.env.REACT_APP_CLOUDINARY_CLOUDNAME} publicId="portfolio/landing_avatar_jpg" alt="Landing Avatar"width="350" height="350" radius="max" />
-          </picture>
+          <Image id="landing-avatar" cloudName={process.env.REACT_APP_CLOUDINARY_CLOUDNAME} publicId="portfolio/landing_avatar_jpg" alt="Landing Avatar" >
+            <Transformation quality="auto" fetchFormat="auto" />
+          </Image>
         </Card.ImgOverlay>
       </Card>
     </div>
