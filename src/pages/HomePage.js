e87bdb2700181ba43a13da2633ba8631ccc5783a
Markus Mengel <markus.mengelhx@gmail.com>
2020-09-13 01:04:07 +0200

    added width to inital image to minimize file size

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -11,8 +11,8 @@ function HomePage(props) {
   return(
     <div id="homepage" >
       <Card id="landing-card" className="bg-dark">
-        <Image className="card-img" id="landing-img" cloudName={process.env.REACT_APP_CLOUDINARY_CLOUDNAME} publicId="portfolio/landing_image_jpg" alt="Landing Image">
-          <Transformation quality="auto" fetchFormat="auto" />
+        <Image className="card-img" id="landing-img" cloudName={process.env.REACT_APP_CLOUDINARY_CLOUDNAME} publicId="portfolio/landing_image" alt="Landing Image">
+          <Transformation quality="auto" fetchFormat="auto" width="1500" crop="scale" />
         </Image>
         <Card.ImgOverlay id="landing-card-overlay" >
           <Card.Text> Hello, I'm </Card.Text>
@@ -21,8 +21,8 @@ function HomePage(props) {
           <Card.Text id="landing-card-subtitle" >{props.subtitle}</Card.Text>
           <Button className="landing-btn" variant="info" size="lg" href="/projects">My Projects</Button>
           <Button className="landing-btn" variant="success" size="lg" href="/contact">Contact Me</Button>
-          <Image id="landing-avatar" cloudName={process.env.REACT_APP_CLOUDINARY_CLOUDNAME} publicId="portfolio/landing_avatar_jpg" alt="Landing Avatar" >
-            <Transformation quality="auto" fetchFormat="auto" />
+          <Image id="landing-avatar" cloudName={process.env.REACT_APP_CLOUDINARY_CLOUDNAME} publicId="portfolio/landing_avatar" alt="Landing Avatar" >
+            <Transformation quality="auto" fetchFormat="auto" width="450" crop="scale" />
           </Image>
         </Card.ImgOverlay>
       </Card>
