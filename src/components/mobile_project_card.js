3a279d2541b4e53a0b335fd098a8758e674fbcf7
Markus Mengel <markus.mengelhx@gmail.com>
2020-09-15 12:08:32 +0200

    removed unneeded code

--- src/components/mobile_project_card.js
+++ src/components/mobile_project_card.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Card from 'react-bootstrap/Card';
-import { Image, Transformation } from 'cloudinary-react';
+import { Image, Video, Transformation } from 'cloudinary-react';
 
 import '../assets/style/mobile_project_card.scss';
 
@@ -10,27 +10,39 @@ class MobileProjectCard extends Component {
     super(props);
     this.state = {
       image: `${this.props.image}_image`,
+      disabled: false,
+      autoplay: false,
+      currenttime: 0
     }
   }
 
   handleMouseEnter = () => {
     this.setState({
-      image: `${this.props.image}_gif`
+      image: `${this.props.image}_gif`,
+      // disabled: true,
+      autoplay: true,
+      currenttime: 0
     })
   };
 
   handleMouseLeave = () => {
     this.setState({
-      image: `${this.props.image}_image`
+      image: `${this.props.image}_image`,
+      disabled: false,
+      autoplay: false
     })
   };
 
   render() {
     return(
       <Card className="mobile-project-card" text="white" onMouseOver={this.handleMouseEnter} onMouseOut={this.handleMouseLeave}>
-        <Image className="card-img mobile-project-image" cloudName={process.env.REACT_APP_CLOUDINARY_CLOUDNAME} publicId={`${this.state.image}`} alt={`${this.props.name} image`}>
-          <Transformation quality="auto" fetchFormat="auto" width={this.state.image.includes("gif") ? "" : "450"} crop={this.state.image.includes("gif") ? "" : "scale"}/>
+        <Image hidden={this.state.disabled} className="card-img " cloudName={process.env.REACT_APP_CLOUDINARY_CLOUDNAME} publicId={this.state.image} resourceType="image" alt={`${this.props.name} image`}>
+          <Transformation quality="auto" fetchFormat={this.state.image.includes("gif") ? "" : "auto"} width={this.state.image.includes("gif") ? "" : "450"} crop={this.state.image.includes("gif") ? "" : "scale"} />
         </Image>
+        {/*<Video ref={this.vidRef} hidden={!this.state.disabled} className="card-img" cloudName={process.env.REACT_APP_CLOUDINARY_CLOUDNAME} publicId={`${this.props.image}_mp4`} alt={`${this.props.name} image`} playsInline muted loop autoPlay >
+                  <Transformation quality="auto" fetchFormat="auto"/>
+                </Video>*/}
+
         <Card.ImgOverlay className="mobile-project-card-img-overlay" >
           <div className="mobile-project-card-text" >
             <Card.Title className="mobile-project-card-header">{this.props.name}</Card.Title>
