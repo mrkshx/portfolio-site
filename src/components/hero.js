50d801f869811ec2a0f2f2ed21daffc460e1dd7f
Markus Mengel <markus.mengelhx@gmail.com>
2020-09-16 14:20:09 +0200

    changed font for hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -10,7 +10,7 @@ import '../assets/style/hero.scss';
 function Hero(props) {
 
   return(
-    <Jumbotron className='bg-transparent Jumbotron-fluid p-0' >
+    <Jumbotron className='bg-transparent Jumbotron-fluid p-0 hero' >
       <Container fluid={true} >
         <Row className='justify-content-center py-5' >
           <Col md={8} sm={3} >
