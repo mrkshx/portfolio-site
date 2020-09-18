6324e63a08a5e41e2586ae6aabc4374f2088d0c7
Markus Mengel <markus.mengelhx@gmail.com>
2020-09-18 12:26:42 +0200

    added invalidate attribute

--- src/components/mobile_project_card.js
+++ src/components/mobile_project_card.js
@@ -32,10 +32,10 @@ class MobileProjectCard extends Component {
   render() {
     return(
       <Card className="mobile-project-card" text="white" onMouseOver={this.handleMouseEnter} onMouseOut={this.handleMouseLeave}>
-        <Image hidden={this.state.disabled} className="card-img " cloudName={process.env.REACT_APP_CLOUDINARY_CLOUDNAME} publicId={`${this.props.image}_image`} alt={`${this.props.name} image`}>
+        <Image hidden={this.state.disabled} className="card-img " cloudName={process.env.REACT_APP_CLOUDINARY_CLOUDNAME} invalidate="true" publicId={`${this.props.image}_image`} alt={`${this.props.name} image`}>
           <Transformation quality="auto" fetchFormat="auto" width="450" crop="scale" />
         </Image>
-        <Video hidden={!this.state.disabled} className="card-img" cloudName={process.env.REACT_APP_CLOUDINARY_CLOUDNAME} publicId={`${this.props.image}_mp4`} alt={`${this.props.name} image`} playsInline muted loop autoPlay >
+        <Video hidden={!this.state.disabled} className="card-img" cloudName={process.env.REACT_APP_CLOUDINARY_CLOUDNAME} invalidate="true" publicId={`${this.props.image}_mp4`} alt={`${this.props.name} image`} playsInline muted loop autoPlay >
           <Transformation quality="auto" fetchFormat="auto"/>
         </Video>
 
